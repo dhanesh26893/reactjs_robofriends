777e879c0a090ea0bc1ae9772cdc3dc4db72f9ec
dhanesh26893 <webdevdhaneshbhandari@gmail.com>
2022-02-10 11:52:53 +0530

    hooks added

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,35 +1,44 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super()
-    this.state = {
-      robots: [],
-      searchfield: '',
+function App () {
+    // constructor() {
+    //   super()
+    //   this.state = {
+    //     robots: [],
+    //     searchfield: '',
+    //   }
+    // }
+
+    const [robots,setRobots] = useState([]);
+    // here useState returns us robots which is a piece of state here its initialized to [] and a function which changes that state
+    const [searchfield,setSearchfield] = useState('');
+    const [count,setCount] = useState(0);
+    // componentDidMount() {
+    //   fetch('https://jsonplaceholder.typicode.com/users')
+    //     .then(response => response.json())
+    //     .then(users => this.setState({ robots: users }))
+    //   // this.setState({robots:this.state.robots});
+    // }
+    useEffect(()=>{
+      fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(users => {setRobots(users)})
+        console.log("count : "+count)
+    },[count]) //only run if count changes
+
+
+    const onSearchChange = (event) => {
+      setSearchfield(event.target.value)
     }
-  }
-
-
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ robots: users }))
-    // this.setState({robots:this.state.robots});
-  }
-
-  onSearchChange = (event) => {
-    this.setState({ searchfield: event.target.value })
-  }
 
 
 
-  render() {
-    const {robots,searchfield} = this.state;
+    // const {robots,searchfield} = this.state;
     const filteredRobots = robots.filter(robot => {
       return robot.name.toLowerCase().includes(searchfield.toLowerCase());
     })
@@ -44,7 +53,8 @@ class App extends Component {
       (     
         <div className='tc'>
           <h1 className='f2'>RoboFriends</h1>
-          <SearchBox searchChange={this.onSearchChange} />
+          <button onClick={()=>setCount(count+1)}>Click Me!</button>
+          <SearchBox searchChange={onSearchChange} />
           <Scroll>
             <ErrorBoundry>
               <CardList robots={filteredRobots} />
@@ -55,7 +65,6 @@ class App extends Component {
 
     }
 
-  }
 
 
 
